ec9ed70502da9527979610f21e455196b63b9b45
Ruthvik Penumatcha <77ruthvik@gmail.com>
2025-05-12 19:09:42 +0000

    Creating the GrepApp Interface

--- core_java/grep/src/main/java/ca/jrvs/apps/practice/HelloWorld.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/HelloWorld.java
@@ -1,7 +1,10 @@
 package ca.jrvs.apps.practice;
 
+import java.util.ArrayList;
+
 class HelloWorld {
 
+  ArrayList<String> arr = new ArrayList<>();
   // Your program begins with a call to main().
   // Prints "Hello, World" to the terminal window.
   public static void main(String[] args) {
